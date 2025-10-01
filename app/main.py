1c41664c656826ba567c3782aa3d8bac3f44c663
Nebeck29 <ndavila@uch.edu.pe>
2025-09-30 21:38:16 -0500

    segundo

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from .database import Base, engine
-from . import models
 from .routes import router
 
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SmartLogix API")
 app.include_router(router)
